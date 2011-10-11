e99825138e094c0303566fee57237f0d896e9db9
Michael Haschke <haschek@eye48.com>
2011-10-11 03:57:45 +0200

    [add] fancybox: a lightbox popup js

---
+++ scripts/settings/media.dev.js
@@ -0,0 +1,12 @@
+$(document).ready( function() {
+    
+    $('a.lightbox').fancybox({
+        'width':    '75%',
+        'height':   '75%',
+        'centerOnScroll': true,
+        'overlayOpacity':   0.3,
+        'overlayColor':     '#69c',
+        'titlePosition': 'inside'
+    });
+    
+});
