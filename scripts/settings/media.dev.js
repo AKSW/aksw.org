5b02ada1af8a4710ea67a6e1e9a94250b7f399c2
Michael Haschke <haschek@eye48.com>
2011-10-12 15:05:55 +0200

    [add] combine js, css and example markup for the demowheel

--- scripts/settings/media.dev.js
+++ scripts/settings/media.dev.js
@@ -31,5 +31,26 @@ $(document).ready( function() {
             });
         }
     });
+    
+    $('div.demowheel > ul').each(function() {
+        var demowheel_items = $(this).children('li');
+        if (demowheel_items.get().length > 1)
+        {
+            demowheel_items.css('width', demowheel_items.width()+'px').addClass('rs-carousel-item').removeClass('first');
+            demowheel_items.css('padding-left', ((demowheel_items.innerWidth() - demowheel_items.width())/2)+'px');
+            demowheel_items.css('padding-right', ((demowheel_items.innerWidth() - demowheel_items.width())/2)+'px');
+            $(this).css('width', (demowheel_items.get().length * demowheel_items.outerWidth())+'px').addClass('rs-carousel-runner');
+            
+            $(this).parent('div.demowheel').carousel({
+                // itemsPerPage: 1,
+                // itemsPerTransition: 1,
+                // pagination: false
+                speed: 'slow',
+                autoScroll: true,
+                pause: 6000
+            });
+        }
+    });
+    
 
 });
